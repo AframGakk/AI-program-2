c567030912587c8fa8933ea09a880686dc0d8332
villi <vilhjalmur12@ru.is>
2019-02-13 15:12:58 +0000

    state is in

---
+++ src/State.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class State {
+
+    private Environment environment;
+    private Player player;
+
+
+    public List<Action> legalActions() {
+        List<Action> retList = new ArrayList<>();
+
+        if (this.player == Player.WHITE) {
+
+
+        } else {
+
+        }
+
+        return null;
+    }
+
+}
